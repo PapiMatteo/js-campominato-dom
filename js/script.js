50d63438db9b7f87198fdd2cbcc8f6559f04dbd0
PapiMatteo <matteopapi.work@gmail.com>
2023-10-30 14:37:31 +0100

    add: bomb generator completed and cell click WIP

--- js/script.js
+++ js/script.js
@@ -4,10 +4,15 @@ const btnElem  = document.querySelector(".start");
 const gridElem = document.querySelector(".grid");
 
 
+let bombList      = [];
+let maxClicks;
+
+
 
 // LOGICA
 
 btnElem.addEventListener("click", handleBtnClick);
+
 
 /***********************/
 // FUNZIONI
@@ -16,8 +21,8 @@ function createCells(innerNumber, cellSize) {
 
     const newCell = document.createElement("div");
     newCell.classList.add("cell");
-    newCell.style.width =`calc(100% / ${cellSize})`;
-    newCell.innerHTML = innerNumber;
+    newCell.style.width = `calc(100% / ${cellSize})`;
+    newCell.innerHTML   = innerNumber;
 
     return newCell;
 
@@ -26,34 +31,37 @@ function createCells(innerNumber, cellSize) {
 function handleBtnClick() {
 
     gridElem.innerHTML = ""
-    const level        = document.querySelector("#level").value;
+    const level = document.querySelector("#level").value;
     let gridSize;
-    let cellSize; 
+    let cellSize;
 
     switch (level) {
         case "1":
             gridSize = 100;
             cellSize = 10;
             break
-        
+
         case "2":
             gridSize = 81;
             cellSize = 9;
             break
-        
+
         case "3":
             gridSize = 49;
             cellSize = 7;
             break
-        
-        default :
+
+        default:
             gridSize = 100;
             cellSize = 10;
             break
     }
 
-    const bombs = bombGenerator(100);
-    console.log(bombs)
+    bombList = bombGenerator(gridSize);
+    console.log(bombList);
+
+    maxClicks = gridSize - bombList.length;
+    console.log(maxClicks);
 
     for (let i = 1; i <= gridSize; i++) {
 
@@ -63,30 +71,43 @@ function handleBtnClick() {
 
         gridElem.append(cell);
         gridElem.classList.add("show");
-        
+
     }
 }
 
-function handleCellClick(){
+function handleCellClick() {
 
     const clickedNumber = parseInt(this.textContent);
     console.log(clickedNumber);
-    this.classList.add("azure");
+    const clickedList = [];
 
+    if (clickedList.length === maxClicks) {
+        console.log("Hai Vinto")
+    } else if (bombList.includes(clickedNumber)) {
+        this.classList.add("red");
+        console.log("Hai Perso");
+
+    } else {
+        this.classList.add("azure");
+
+        if (!clickedList.includes(clickedNumber) && !bombList.includes(clickedNumber)){
+            clickedList.push(clickedNumber)
+        }
+    }
+    
 }
 
-function generateRndNum (min, max) {
+function generateRndNum(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-function bombGenerator (max) {
+function bombGenerator(max) {
     const result = [];
 
-    while (result.lenght < 16) {
+    while (result.length < 16) {
         const newNum = generateRndNum(1, max)
-        console.log(newNum)
 
-        if (!result.includes(newNum)){
+        if (!result.includes(newNum)) {
             result.push(newNum);
         }
     }
